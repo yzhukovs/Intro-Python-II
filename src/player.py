1b4586763ee458e8846ba90974975df9b827df3d
yzhukovs <yzhukovs@bumnetworks.com>
2019-03-21 16:06:16 -0400

    refactoring

--- src/player.py
+++ src/player.py
@@ -1,21 +1,34 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+from item import Item
+
 class Player:
     def __init__(self, name, starting_room):
         self.name = name
         self.current_room = starting_room
         self.items = []
     def moving(self, direction):
-        if direction in ["n", "N","s","S","e", "E", "w", "W"]:
+        if direction in ["n", "s", "e", "w"]:
             next_room = self.current_room.get_direction(direction)
             if next_room is not None:
                 self.current_room = next_room
-            else: 
-                print("You cannot move in that direction")
+                print(self.current_room)
+            else:
+                print("You cannot move in that direction.")
 
 
     def add_item(self, item):
         self.items.append(item)
 
     def remove_item(self, item):
-        self.items.remove(item)    
+        for i in self.items:
+            if i.name == item:
+                self.items.remove(i)  
+
+    def inventory(self):
+        if len(self.items) == 0:
+            return "No items in inventory."
+        else: 
+            names = [item.name for item in self.items]
+            return f"Items in your inventory: {', '.join(names)}"
--- src/room.py
+++ src/room.py
@@ -13,7 +13,25 @@ class Room:
         self.e_to = None
         self.w_to = None
     def __repr__(self):
-        return f"{self.name}\n\n {self.description}\n\n You have: {self.items}"
+        room_string = f"{self.name}\n\n"
+        room_string += f"{self.description}\n\n"    
+        room_string += f"Exits: {self.get_exits()}"
+        if len(self.items) > 0:
+            room_string += f"You see {self.get_item_string()}"      
+        return room_string
+    def get_item_string(self):
+        return ','.join([str(i) for i in self.items])    
+    def get_exits(self):
+        exits = []
+        if self.n_to is not None:
+            exits.append('n')
+        if self.e_to is not None:
+            exits.append('e')
+        if self.s_to is not None:
+            exits.append('s') 
+        if self.w_to is not None:
+            exits.append('w')  
+        return ", ".join(exits)     
     def get_direction(self, direction):
         if direction == "n":
             return self.n_to
@@ -25,13 +43,23 @@ class Room:
             return self.w_to
         else:
             return None   
-     
+    def find_item_by_string(self, item_name):
+        item_names = [item.name for item in self.items]
+        print(item_names)
 
     def add_item(self, item):
-        self.items.append(item)
+        item_to_find_name = item
+        for item in [i.lower() for i in self.items]:
+            if item.name == item_to_find_name.lower():
+                self.items.append(item)
+                break       
 
     def remove_item(self, item):
-        self.items.remove(item)        
+        item_to_find_name = item
+        for item in [i.lower() for i in self.items]:
+            if item.name == item_to_find_name.lower():
+                self.items.remove(item)
+                break        
 
 #class Room(Empty_Room):
 #   def __init__(self, name, description, *items):
